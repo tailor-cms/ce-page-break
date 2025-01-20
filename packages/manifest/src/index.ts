5b03360202357d99ab87023e748aaabc4cb7c2e3
rkusan00 <renato.kusanovic@gmail.com>
2025-01-20 14:31:07 +0100

    Remove CE prefix from type

--- packages/manifest/src/index.ts
+++ packages/manifest/src/index.ts
@@ -5,7 +5,7 @@ import type {
 } from './interfaces';
 
 // Element unique id within the target system (e.g. Tailor)
-export const type = 'CE_PAGE_BREAK';
+export const type = 'PAGE_BREAK';
 
 // Display name (e.g. shown to the author)
 export const name = 'Section Break';
