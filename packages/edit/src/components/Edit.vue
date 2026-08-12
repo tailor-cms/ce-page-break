1bae25eef03cef5c107578f94a19feb91f7f3705
rkusan00 <renato.kusanovic@gmail.com>
2026-08-12 16:06:06 +0200

    Cleanup

--- packages/edit/src/components/Edit.vue
+++ packages/edit/src/components/Edit.vue
@@ -1,7 +1,11 @@
 <template>
-  <VDivider aria-label="Section break" opacity="0.5" variant="dashed">
-    <span class="text-label-large text-medium-emphasis text-uppercase">
-      Section break
-    </span>
-  </VDivider>
+  <div class="tce-page-break">
+    <VDivider aria-label="Section break" opacity="0.5" variant="dashed">
+      <span
+        class="text-label-medium text-medium-emphasis text-uppercase font-weight-semibold"
+      >
+        Section break
+      </span>
+    </VDivider>
+  </div>
 </template>
