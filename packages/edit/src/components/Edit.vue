a1d8958c7fc535821cc9299aac1caf59f0d2b757
rkusan00 <renato.kusanovic@gmail.com>
2026-08-12 15:44:21 +0200

    Cleanup

--- packages/edit/src/components/Edit.vue
+++ packages/edit/src/components/Edit.vue
@@ -1,15 +1,7 @@
 <template>
-  <VSheet class="tce-page-break pa-1" color="primary-darken-3">
-    Section break
-    <VIcon class="ml-4 pb-1" color="secondary-lighten-2" size="24">
-      mdi-format-page-break
-    </VIcon>
-  </VSheet>
+  <VDivider aria-label="Section break" opacity="0.5" variant="dashed">
+    <span class="text-label-large text-medium-emphasis text-uppercase">
+      Section break
+    </span>
+  </VDivider>
 </template>
-
-<style lang="scss" scoped>
-.tce-page-break {
-  text-align: center;
-  font-size: 1.125rem;
-}
-</style>
